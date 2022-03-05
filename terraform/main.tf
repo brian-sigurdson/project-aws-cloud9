88762a9bf86c76894de0fd76825795f1ed468777
Brian Sigurdson <sigurdson.brian@gmail.com>
2022-03-04 21:03:46 -0600

    updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,12 +28,12 @@ data "aws_instance" "cloud9_instance" {
 }
 
 # get the latest amazon linux ami
-data "aws_ssm_parameter" "ami" {
-  name = "/aws/service/ami-amazon-linux/amzn2-ami-hvm-x86_64-gp2"
-}
+# data "aws_ssm_parameter" "ami" {
+#   name = "/aws/service/ami-amazon-linux/amzn2-ami-hvm-x86_64-gp2"
+# }
 #####################################################################################################
 # output
 #####################################################################################################
-# output "cloud9_url" {
-#   value = "https://${var.region}.console.aws.amazon.com/cloud9/ide/${aws_cloud9_environment_ec2.example.id}"
-# }
+output "cloud9_url" {
+  value = "https://${var.region}.console.aws.amazon.com/cloud9/ide/${aws_cloud9_environment_ec2.example.id}"
+}
