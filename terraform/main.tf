2f086715306abf96856059c63fe0f515d3d666b6
Brian Sigurdson <sigurdson.brian@gmail.com>
2022-03-05 09:20:26 -0600

    updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,21 @@
-# example code, to practice, taken from
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloud9_environment_ec2
-
 #####################################################################################################
 # providers
 #####################################################################################################
 provider "aws" {
-    # region = "us-east-2"
+
 }
 
 #####################################################################################################
 # resources
 #####################################################################################################
-resource "aws_cloud9_environment_ec2" "example" {
+resource "aws_cloud9_environment_ec2" "my-ide" {
+  name = "c9-ide"
   instance_type = "t2.micro"
-  name = "bks-c9-practice"
   automatic_stop_time_minutes = 30
+  description = "A browser based IDE on AWS."
+  tags = {
+    name = "practice tag"
+  }
 }
 
 #####################################################################################################
@@ -23,17 +24,6 @@ resource "aws_cloud9_environment_ec2" "example" {
 data "aws_instance" "cloud9_instance" {
   filter {
     name = "tag:aws:cloud9:environment"
-    values = [aws_cloud9_environment_ec2.example.id]
+    values = [aws_cloud9_environment_ec2.my-ide.id]
   }
 }
-
-# get the latest amazon linux ami
-# data "aws_ssm_parameter" "ami" {
-#   name = "/aws/service/ami-amazon-linux/amzn2-ami-hvm-x86_64-gp2"
-# }
-#####################################################################################################
-# output
-#####################################################################################################
-# output "cloud9_url" {
-#   value = "https://${var.region}.console.aws.amazon.com/cloud9/ide/${aws_cloud9_environment_ec2.example.id}"
-# }
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,19 @@
 output "cloud9_url" {
-  value = "https://${var.region}.console.aws.amazon.com/cloud9/ide/${aws_cloud9_environment_ec2.example.id}"
+  value = "https://${var.region}.console.aws.amazon.com/cloud9/ide/${aws_cloud9_environment_ec2.my-ide.id}"
+}
+
+output "id" {
+  value = aws_cloud9_environment_ec2.my-ide.id
+}
+
+output "arn" {
+  value = aws_cloud9_environment_ec2.my-ide.arn
+}
+
+output "tags_all" {
+  value = aws_cloud9_environment_ec2.my-ide.tags_all
+}
+
+output "type" {
+  value = aws_cloud9_environment_ec2.my-ide.type
 }
